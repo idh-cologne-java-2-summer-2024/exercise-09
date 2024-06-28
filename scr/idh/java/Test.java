0a98f9c233fd177aa65e6150d35d73d10f953811
limpl <limpl@DESKTOP-NCMTBB0>
2024-06-28 15:06:16 +0200

    Soweit so gut

---
+++ scr/idh/java/Test.java
@@ -0,0 +1,16 @@
+package idh.java;
+import org.apache.commons.collections4.list.TreeList;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Test {
+	
+	TreeList<Integer> treeList = new TreeList<>();
+	ArrayList<Integer> arrayList = new ArrayList<>();
+	LinkedList<Integer> linkedList = new LinkedList<>();
+	
+	
+	
+	
+
+}
