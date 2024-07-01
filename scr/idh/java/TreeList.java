1bf085c2b5524005df53437f19457fdeae1442af
pauls <pauls@PsPC.fritz.box>
2024-07-01 18:31:34 +0200

    .

--- scr/idh/java/TreeList.java
+++ scr/idh/java/TreeList.java
@@ -1,13 +1,12 @@
 package idh.java;
 
 import java.util.AbstractList;
-
+import java.util.ListIterator;
+import java.util.Random;
 
 public class TreeList  extends AbstractList{
 
-	TreeList() {
-		
-	}
+	
 
 	@Override
 	public int size() {
@@ -20,4 +19,33 @@ public class TreeList  extends AbstractList{
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	public static void main(String args) {
+               final TreeList<Integer> list = new TreeList<Integer>(3);
+               
+               for (int i = 0; i < 100; ++i) {
+                   list.add(i);
+                }
+              for (int i = 0; i < 100; ++i) {
+                 System.out.println(i + ": " + list.get(i));
+        }
+	}
+	
+	/**
+     * Loads a list with random integers and returns the duration of loading in
+     * milliseconds.
+     * 
+     * @param list the list to load.
+     * @param rnd  the random number generator.
+     * @return     the duration for loading in milliseconds.
+     */
+    private static long load(final AbstractList<Integer> list, final Random rnd) {
+        final long ta = System.currentTimeMillis();
+
+        for (int i = 0; i < LOAD; ++i) {
+            final int index = rnd.nextInt(list.size() + 1);
+            list.add(index, rnd.nextInt());
+        }
+            return System.currentTimeMillis() - ta;
+        }
 }
