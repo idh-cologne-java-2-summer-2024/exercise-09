324f4998a900b1a599a5a1492002678bf5ee5cc1
pauls <paul.speier@web.de>
2024-07-01 13:23:38 +0200

    Aufgabe 1

---
+++ scr/idh/java/TreeList.java
@@ -0,0 +1,23 @@
+package idh.java;
+
+import java.util.AbstractList;
+
+
+public class TreeList  extends AbstractList{
+
+	TreeList() {
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
