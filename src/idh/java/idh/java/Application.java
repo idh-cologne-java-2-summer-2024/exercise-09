d14132e329b7dcd5b0c86a048c1862fdc596b510
oona- <oona-@192.168.178.156>
2024-07-02 22:31:43 +0200

    Exercise 9

--- src/idh/java/idh/java/Application.java
+++ src/idh/java/idh/java/Application.java
@@ -1,5 +1,16 @@
 package idh.java;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import org.apache.commons.collections4.list.TreeList;
+// ich weiß nicht, wieso das oben rot angestrichen ist, eiegntlich müsste es laut Demmer so richtig sein.
 public class Application {
 
+	public static void main(String[] args) {
+		
+		System.out.println("ArrayList: " + ListTest.getTime(new ArrayList<Integer>()) + "ms");
+		System.out.println("TreeList: " + ListTest.getTime(new TreeList()) + "ms");
+		System.out.println("LinkedList: " + ListTest.getTime(new LinkedList<Integer>()) + "ms");
+	}
+	
 }
--- src/idh/java/idh/java/ListTest.java
+++ src/idh/java/idh/java/ListTest.java
@@ -1,5 +1,27 @@
 package idh.java;
 
+import java.util.List;
+import java.util.Random;
+
 public class ListTest {
 
+	
+	public static long getTime(List<Integer> listToTest) {
+		
+		long startTime = System.currentTimeMillis();
+		
+		Random random = new Random();
+		for (int i = 0; i < 100000; i++) {
+			listToTest.add(random.nextInt());
+		}
+		
+		random = new Random();
+		for (int i = 0; i < 100000; i++) {
+			listToTest.add(random.nextInt(99999), random.nextInt());
+		}
+		
+		
+		return System.currentTimeMillis() - startTime;
+	}
+	
 }
